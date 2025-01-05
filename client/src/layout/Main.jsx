b3822264cdb7e8c4646620dd01efa3d0d07ec4cc
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2025-01-05 07:32:42 +0600

    basic setup

---
+++ client/src/layout/Main.jsx
@@ -0,0 +1,17 @@
+
+
+const Main = () => {
+    return (
+        <div>
+            {/* navbar */}
+            <h1>Navbar</h1>
+
+            {/* Outlet */}
+
+            {/* Footer */}
+            
+        </div>
+    );
+};
+
+export default Main;
