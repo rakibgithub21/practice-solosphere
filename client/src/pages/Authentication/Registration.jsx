9fef6b962016028d990aa133f399749a10bbb5bb
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2025-01-06 12:46:47 +0600

    make logout functionality

--- client/src/pages/Authentication/Registration.jsx
+++ client/src/pages/Authentication/Registration.jsx
@@ -1,6 +1,25 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { AuthContext } from "../../provider/AuthProvider"
+import toast from "react-hot-toast"
 
 const Registration = () => {
+    const { createUser,user,setUser, updateUserProfile, signInWithGoogle } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    //google sign in
+
+    const handleGoogleSignIn = async () => {
+        try {
+            await signInWithGoogle()
+            toast.success('success fully logged in')
+            navigate('/')
+        } catch (error) {
+            console.log(error);
+            toast.error(error?.message)
+        }
+    }
+
     return (
         <div className='flex my-12 justify-center items-center min-h-[calc(100vh-306px)]'>
             <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl '>
@@ -17,7 +36,7 @@ const Registration = () => {
                         Get Your Free Account Now.
                     </p>
 
-                    <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+                    <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
                         <div className='px-4 py-2'>
                             <svg className='w-6 h-6' viewBox='0 0 40 40'>
                                 <path
